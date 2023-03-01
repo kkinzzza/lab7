3e0f18813aa13276c73b76b95d0ec557d8a172e1
Kirill Dzhenzherukha <112971844+kkinzzza@users.noreply.github.com>
2023-03-01 20:57:30 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -3,9 +3,9 @@ import time
 import random
 
 t_start = time.perf_counter()  # обозначаем время начала работы
-for i in range(1000001):
+for i in range(1000001):  # создаем массивы из 1 млн случайных чисел
     s1 = np.array(random.uniform(1, 10 ** 10), float)
-    s2 = np.array(random.uniform(1, 10 ** 10), float)  # создаем массивы из 1 млн случайных чисел
+    s2 = np.array(random.uniform(1, 10 ** 10), float)
 
 print(np.multiply(s1, s2))  # печатаем результат работы функции np.multiply()
 print(time.perf_counter() - t_start)  # печатаем время работы алгоритма
