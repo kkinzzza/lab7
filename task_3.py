1b849597b07fc1d9046b428fad464cc1821a88fa
Kirill Dzhenzherukha <112971844+kkinzzza@users.noreply.github.com>
2023-03-01 20:55:22 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -6,9 +6,9 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 fig = plt.figure()  # создаем фигуру
-ax = Axes3D(fig)  # создаем трехмерную систему координат 
+ax = Axes3D(fig)  # создаем трехмерную систему координат
 # ax = fig.add_subplot(projection='3d')  # альтернативный метод создания 3-хмерной СК
-x = np.linspace(-5*np.pi, 5*np.pi)  # интервалы согласно условию задачи
+x = np.linspace(-5 * np.pi, 5 * np.pi)  # интервалы согласно условию задачи
 y = np.cos(x)  # задаем функциональные зависимости для осей y и z
 z = np.sin(x)
 ax.plot(x, y, z, color='darkblue')  # строим трехмерный график по заданию
@@ -16,4 +16,4 @@ ax.set_xlabel('x')  # указываем названия осей
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 fig.suptitle('График функции:', fontsize=14)  # указываем заголовок
-plt.show() 
+plt.show()
