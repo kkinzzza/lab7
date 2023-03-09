78880a086468500ab37c249d4e47425342b9154d
Kirill Dzhenzherukha <112971844+kkinzzza@users.noreply.github.com>
2023-03-09 22:32:45 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -14,24 +14,19 @@ with open('data1.csv', encoding='utf8') as file:  # открываем файл,
         throttle_valve = np.append(throttle_valve, int(row[3]))
         fuel_consumption = np.append(fuel_consumption, float(row[17]))
     fig = plt.figure()  # создаем окно
-    ax1 = plt.subplot(221)  # добавляем систему координат для графика, в скобках указываем позицию в окне
-    ax1.plot(time, throttle_valve, color='red')  # создаем график зависимости положения дрос. засл. от времени
+    ax1 = plt.subplot(121)  # добавляем систему координат для графика, в скобках указываем позицию в окне
+    ax1.plot(time, throttle_valve, time, fuel_consumption)  # создаем графики зависимостей параметров от времени
     ax1.grid()  # на систему координат накладываем сетку
-    ax1.set_xlabel('Время', fontsize=9)  # подписываем оси
-    ax1.set_ylabel('Положение дроссельной заслонки (%)', fontsize=9)
+    ax1.set_xlabel('Время', fontsize=10)  # подписываем оси
+    ax1.set_ylabel('Значение параметра: \n синий - положение дроссельной заслонки, \n '
+                   'оранжевый - часовой расход топлива', fontsize=10)
     # далее - аналогично
-    ax2 = plt.subplot(222)
-    ax2.plot(time, fuel_consumption, color='orange')
+    ax2 = plt.subplot(122)
+    ax2.scatter(throttle_valve, fuel_consumption, color='pink', edgecolor='darkblue')  # создаем график корреляции
     ax2.grid()
-    ax2.set_xlabel('Время', fontsize=9)
-    ax2.set_ylabel('Часовой расход топлива (л/час)', fontsize=9)
-    ax3 = plt.subplot(212)
-    ax3.scatter(time, throttle_valve, color='darkblue')  # с помощью метода scatter создаем точечный график корреляции
-    ax3.scatter(time, fuel_consumption, color='green')
-    ax3.grid()
-    ax3.set_xlabel('Время', fontsize=10)
-    ax3.set_ylabel('Значение параметра: \n синий - положение дроссельной заслонки, \n '
-                   'зеленый - часовой расход топлива', fontsize=9)
-    fig.suptitle('Графики зависимости положения дроссельной заслонки \n и '
-                 'часового расхода топлива от времени', fontsize=14)  # записываем заголовок окна
-    plt.show()  # команда необходима для показа окна с графиками
+    ax2.set_xlabel('Положение дроссельной заслонки', fontsize=10)
+    ax2.set_ylabel('Часовой расход топлива', fontsize=10)
+fig.suptitle('Графики зависимостей положения дроссельной заслонки \n '
+             'и часового расхода топлива от времени \n'
+             'и график их корреляции:')
+plt.show()  # команда необходима для показа окна с графиками
