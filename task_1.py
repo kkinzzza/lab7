8c8d9fbb806bdb8636708146a21f24d6f63b8808
Kirill Dzhenzherukha <112971844+kkinzzza@users.noreply.github.com>
2023-03-09 22:33:07 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,11 +1,16 @@
 import numpy as np
 import time
-import random
+from random import uniform
 
 t_start = time.perf_counter()  # обозначаем время начала работы
-for i in range(1000001):  # создаем массивы из 1 млн случайных чисел
-    s1 = np.array(random.uniform(1, 10 ** 10), float)
-    s2 = np.array(random.uniform(1, 10 ** 10), float)
 
-print(np.multiply(s1, s2))  # печатаем результат работы функции np.multiply()
+s1 = np.random.uniform(1, 10 ** 10, 1000001)  # создаем два массива, заполненные миллионом случайных чисел
+s2 = np.random.uniform(1, 10 ** 10, 1000001)
+result = np.multiply(s1, s2)  # выполняем операцию поэлементного перемножения массивов
+
+# # аналогично для стандартных массивов без использования numpy
+# s1, s2, result = [uniform(1, 10 ** 10) for i in range(1000001)], [uniform(1, 10 ** 10) for j in range(1000001)], []
+# for i in range(1000001):
+#     result.append(s1[i] * s2[i])
+
 print(time.perf_counter() - t_start)  # печатаем время работы алгоритма
